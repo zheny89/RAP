a26e32795b35c49181c6abdc29a504aa04d23430
ekiselev <ekiselev@rusoft-veu.chel.rusoft.local>
2017-06-27 10:12:17 +0500

    Lalka

---
+++ Rap/src/rap/BasicEntryPoint.java
@@ -0,0 +1,21 @@
+package rap;
+
+import org.eclipse.rap.rwt.application.AbstractEntryPoint;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+
+public class BasicEntryPoint extends AbstractEntryPoint {
+
+    @Override
+    protected void createContents(Composite parent) {
+        parent.setLayout(new GridLayout(2, false));
+        Button checkbox = new Button(parent, SWT.CHECK);
+        checkbox.setText("Hello");
+        Button button = new Button(parent, SWT.PUSH);
+        button.setText("World");
+    }
+
+}
