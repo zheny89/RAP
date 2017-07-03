d4a41d541d10c2c5e49ce45fae22207d91ee47c6
Anton <budowniczy.zamyslow@gmail.com>
2017-07-03 09:02:10 +0500

    Add to prev commit

--- Rap/src/database/WorktimeKey.java
+++ Rap/src/database/WorktimeKey.java
@@ -1,14 +1,62 @@
 package database;
 
-import lombok.Data;
 import java.io.Serializable;
 import java.sql.Date;
 
 @SuppressWarnings("serial")
-@Data
 public class WorktimeKey implements Serializable {
 	
 	private Date day;
 	private final int worker;
+	
+	public WorktimeKey(int workerId) {
+		this.worker = workerId;
+	}
 
+	public Date getDay() {
+		return day;
+	}
+
+	public void setDay(Date day) {
+		this.day = day;
+	}
+
+	public int getWorker() {
+		return worker;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((day == null) ? 0 : day.hashCode());
+		result = prime * result + worker;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WorktimeKey other = (WorktimeKey) obj;
+		if (day == null) {
+			if (other.day != null)
+				return false;
+		} else if (!day.equals(other.day))
+			return false;
+		if (worker != other.worker)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "WorktimeKey [day=" + day + ", worker=" + worker + "]";
+	}
+	
+	
 }
